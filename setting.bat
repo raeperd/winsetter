ba0246980167176d5ae2072d86deb2332f3b26a6
reyzy <rach7089@gmail.com>
2019-09-18 19:30:09 +0900

    First commit

---
+++ setting.bat
@@ -0,0 +1,25 @@
+choco install chocolatey -y
+
+choco install python --version 3.6.7 -y 
+choco install jre8 -y
+choco install autohotkey.portable -y
+
+choco install googlechrome -y
+
+choco install git -y
+choco install sourcetree -y
+choco install tortoisegit -y
+
+choco install dependencywalker -y
+choco install procexp -y
+
+choco install vscode -y
+choco install typora -y
+choco install notepadplusplus -y
+
+choco install virtualbox -y
+
+choco install telegram -y
+
+choco install bandizip -y
+choco install wox -y 
